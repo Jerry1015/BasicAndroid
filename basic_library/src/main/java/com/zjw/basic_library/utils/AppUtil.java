7e2a788e604fdde1779ca78527a110534f72dda7
Jared-PC-D <1363946512@qq.com>
2020-05-27 19:51:09 +0800

    3

--- basic_library/src/main/java/com/zjw/basic_library/utils/AppUtil.java
+++ basic_library/src/main/java/com/zjw/basic_library/utils/AppUtil.java
@@ -7,4 +7,9 @@ public class AppUtil {
     public static void getH(Context context){
         Toast.makeText(context,"hahaha",Toast.LENGTH_SHORT).show();
     }
+
+
+    public static void test2(Context context){
+        Toast.makeText(context,"test2",Toast.LENGTH_SHORT).show();
+    }
 }
